6d2ade34aaf4bb0c1f06eb80e51583ec0ff94211
joaovictor3g <jvdias79797@gmail.com>
2020-02-24 20:20:16 -0300

    teste

--- testandoCPP/exercSlides/imprimirInvertido.cpp
+++ testandoCPP/exercSlides/imprimirInvertido.cpp
@@ -5,13 +5,10 @@ int main() {
     std::cin >> n;
 
     int *vetor = new int[n];
-
     for(int i = 0; i < n; i++)      std::cin >> *(vetor+i);
 
     for(int j = n - 1; j >= 0; j--)      std::cout << *(vetor+j) << " ";
-
     std::cout << "\n";
-
 
     return 0;
 }
