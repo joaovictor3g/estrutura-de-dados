6b6d4a286e1af099021ee89c26f650ce1dc1fffd
joaovictor3g <jvdias79797@gmail.com>
2020-02-24 19:15:08 -0300

    implementando strings

---
+++ testandoCPP/exercSlides/copiarStrings.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "lib/copiarStrings.h"
+
+char *strcpy(char *destino, char *origem) {
+    
+    for(int i = 0; origem[i]; i++) 
+        destino[i] = origem[i];
+    return destino;
+}
+
+int strcmp(char *str1, char *str2) {
+    int cont1{0}, cont2{0};
+
+    while(str1[cont1])  cont1++;
+    while(str2[cont2])  cont2++;
+
+    if(cont1 == cont2)  return 0;
+    else if(cont1 < cont2)  return -1;
+    else return 1;
+    return 2;
+}
+
+int main() {
+    char str1[]{"Ola gente"};
+    char str2[40];
+
+    std::cout << strcpy(str2, str1) << std::endl;
+
+    char str3[]{"ola!"};
+    char str4[]{"ola"};
+    std::cout << strcmp(str3, str4) << std::endl;
+    
+    
+    return 0;
+}
