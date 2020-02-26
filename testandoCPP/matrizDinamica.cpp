b1712e961cce56b626c65da68d0dc12a5ba88d05
joaovictor3g <jvdias79797@gmail.com>
2020-02-26 16:39:51 -0300

    atualizando matriz

--- testandoCPP/matrizDinamica.cpp
+++ testandoCPP/matrizDinamica.cpp
@@ -1,5 +1,36 @@
 #include <iostream>
 
+void ordenarLinhaOuColunaCrescenteEDecrescente(int **matriz, int linhas, int colunas) {
+    bool ordenado = false;
+
+    while(!ordenado) {
+        ordenado = true;
+        for(int i = 0; i < linhas-1; i++) {
+            for(int j = 0; j < colunas; j++) {
+                if(matriz[i][j] > matriz[i+1][j]){
+                    ordenado = false;
+                    int aux = matriz[i][j];
+                    matriz[i][j] = matriz[i+1][j];
+                    matriz[i+1][j] = aux;
+                }
+            }
+        }
+    }
+    for(int i = 0; i < linhas; i++) {
+        for(int j = 0; j < colunas; j++) {
+            std::cout << matriz[i][j] << " ";
+        }
+        std::cout << "\n";
+    }
+    std::cout << "\n";
+    for(int i = linhas - 1; i >= 0; i--) {
+        for(int j = colunas - 1; j >= 0; j--) {
+            std::cout << matriz[i][j] << " ";
+        }
+        std::cout << "\n";
+    }
+}
+
 int main() {
     int linhas = 0, colunas = 0;
     std::cin >> linhas >> colunas;
@@ -13,13 +44,16 @@ int main() {
             std::cin >> matriz[i][j];
        
     }
-
+    std::cout << "\n"; 
     for(int i = 0; i < linhas; i++) {
         for(int j = 0; j < colunas; j++) 
             std::cout << matriz[i][j] << " ";
         std::cout << "\n";
        
     }
+    std::cout<<"\n";
+
+    ordenarLinhaOuColunaCrescenteEDecrescente(matriz, linhas, colunas);
 
     for(int i = 0; i < 3; i++)
         delete[] matriz[i];
