7b28b6bc67948cd727a873241aee08d691019189
joaovictor3g <jvdias79797@gmail.com>
2020-03-07 16:07:51 -0300

    ainda inacabado ;-;

--- ListadeExercicios/questao7.cpp
+++ ListadeExercicios/questao7.cpp
@@ -5,23 +5,46 @@ using std::cin;
 using std::cout;
 using std::endl;
 using std::getchar;
+using std::putchar;
 
 int main() {
-    int size = 10, indice = 0;
-    char *arrChar = new char[size];
-    
-    cout << "Pare digitando ENTER" << endl;
+    int size = 10, indice = 0, ind_max = 10;
+    char *first_array = new char[size];
+    char *third_array = nullptr;
 
-    if(arrChar == nullptr)
-        arrChar = new char[size];
-
-    else {
-        char *ptr = arrChar + size;
-        for(int i = 0; i < size; i++)
-            arrChar[i] = getchar();
+    while((first_array[indice] = getchar()) != '\n') {
+        indice++;
         
+        if(indice >= size) {
+            int i = 0;
+            third_array = new char[1000];
 
+            for(; first_array[i]; i++) 
+                third_array[i] = first_array[i];
+            
+            first_array[++i] = '\0';
+            if(i == 10)
+                delete[] first_array;
+
+        }
     }
-    
+
+    if(indice < size) {
+        first_array[++indice] = '\0';
+        
+        for(int i = 0; first_array[i]; i++) 
+            cout << first_array[i];
+            
+    }
+
+    if(third_array != nullptr) {
+        third_array[++indice] = '\0';
+        
+        for(int i = 0; third_array[i]; i++)
+            cout << third_array[i]; 
+        cout << endl;
+    }
+
+
     return 0;
 }
