156c452f02b9d45759d37d2007b0f2123c6ece67
joaovictor3g <jvdias79797@gmail.com>
2020-03-08 15:50:41 -0300

    agora vai

--- ListadeExercicios/questao4.cpp
+++ ListadeExercicios/questao4.cpp
@@ -10,7 +10,6 @@ int size;
 
 //Preenchendo matriz
 void preencherMatriz(int **matriz, int n) {
-
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < n; j++)
             cin >> matriz[i][j];
@@ -20,17 +19,17 @@ void preencherMatriz(int **matriz, int n) {
 //Função para calcular o valor maximo da matriz
 void max(int **matriz, int n, int *k, int *l, int *c) {
     //declarando um valor inicial muito pequeno para que a primeira condição seja satisfeita sempre
-    *k = -10000000;
+    *k = matriz[0][0];
     int posicaoL = 0, posicaoC = 0;
-
-    //calculo do valor maximo.
+    //calculo do valor maximo e usando os ponteiros l e c para guardar os indices em q k aparece mais de uma vez.
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < n; j++) {
-            if(*k <= matriz[i][j])  *k = matriz[i][j];
-            
+            if(*k <= matriz[i][j])
+                *k = matriz[i][j];
+        
         }
     }
-    //Usando os ponteiros l e c para guardar os indices em q k aparece mais de uma vez
+
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < n; j++) {
             if(*k == matriz[i][j]) {
@@ -38,10 +37,12 @@ void max(int **matriz, int n, int *k, int *l, int *c) {
                 c[posicaoC] = j;
                 posicaoL++;
                 posicaoC++;
-            }
+            }       
         }
     }
-    size = posicaoL;
+
+    //Guardando a quantidade de vezes q o maior elemento aparece
+    size = posicaoC;
 }   
 
 int main() {
@@ -49,32 +50,58 @@ int main() {
     cin >> tamanho;
     //Alocando matriz e vetores dinamicamente
     int **matriz = new int*[tamanho];
-    int *linha = new int[tamanho];
-    int *coluna = new int[tamanho];
+    //Criando dois vetores alocados dinamicamente afim de guardar as posicoes de linha
+    //e coluna do maior elemento da matriz.
+
+    int *vetor_posicao_linha = new int[tamanho*tamanho];
+    int *vetor_posicao_coluna =  new int[tamanho*tamanho]; 
 
     //Alocando coluna da matriz
     for(int i = 0; i < tamanho; i++)
         matriz[i] = new int[tamanho];
     
     preencherMatriz(matriz, tamanho);
-    max(matriz, tamanho, &maior, linha, coluna);
+    max(matriz, tamanho, &maior, vetor_posicao_linha, vetor_posicao_coluna);
     cout << "O maior elemento é: " << maior << endl;
 
     cout << "Coluna(s) e Linha(s) que o "<< maior <<" apareceu: ";
 
     for(int i = 0; i < size; i++)
-        cout << "["<< linha[i] << "]" << "[" << coluna[i] << "]" << " ";
+        cout << "["<< vetor_posicao_linha[i] << "]" << "[" << vetor_posicao_coluna[i] << "]" << " ";
     cout << endl;
 
     //Liberando o espaco alocado
+    delete[] vetor_posicao_coluna;
+    delete[] vetor_posicao_linha;
     for(int i = 0; i < tamanho; i++)
         delete[] matriz[i];
     delete[] matriz;
-    delete[] linha;
-    delete[] coluna;
-    matriz = nullptr;
-    linha = nullptr;
-    coluna = nullptr;
     
     return 0;
 }
+/*
+Testes
+1
+----------->
+3
+1 2 3 
+3 2 1
+9 0 8
+<-----------
+O maior elemento é: 9
+Coluna(s) e Linha(s) que o 9 apareceu: [2][0] 
+
+
+Teste 2
+------------>
+5
+9 9 9 9 9
+9 9 9 9 9
+9 9 9 9 9 
+9 9 9 9 9
+9 9 9 9 9
+<-------------
+O maior elemento é: 9
+Coluna(s) e Linha(s) que o 9 apareceu: [0][0] [0][1] [0][2] [0][3] [0][4] [1][0] [1][1] [1][2] [1][3] [1][4] [2][0] [2][1] [2][2] [2][3] [2][4] 
+[3][0] [3][1] [3][2] [3][3] [3][4] [4][0] [4][1] [4][2] [4][3] [4][4] 
+*/
