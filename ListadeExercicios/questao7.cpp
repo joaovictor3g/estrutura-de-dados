2d7b01a9952c4a66cf7a26d4aec32e2f7dd64ad0
joaovictor3g <jvdias79797@gmail.com>
2020-03-08 14:11:28 -0300

    finalmente

--- ListadeExercicios/questao7.cpp
+++ ListadeExercicios/questao7.cpp
@@ -1,52 +1,40 @@
 #include <iostream>
 #include <cstring>
+#include <cstdio>
 
 using std::cin;
 using std::cout;
 using std::endl;
-using std::getchar;
-using std::putchar;
 
 int main() {
     int size = 10, indice = 0;
     char *first_array = new char[size];
-    char *third_array = new char[1000000];
+    char *third_array = nullptr;
 
     while((first_array[indice] = getchar()) != '\n') {
         indice++;
-        
         if(indice >= size) {
-            int i = 0;
             size += 10;
             third_array = new char[size];
-        
-            for(; first_array[i]; i++) 
+            
+            for(int i = 0; i < size - 10; i++)
                 third_array[i] = first_array[i];
-            first_array[++i] = '\0';
-            
-            if(i == 10)     
-                delete[] first_array;
-            
+			
+            delete[] first_array;
+            first_array = new char[size];
+
+            for(int i = 0; i < size - 10; i++)
+                first_array[i] = third_array[i];
+			
+            delete[] third_array;
+            third_array = nullptr;
         }
     }
 
-    if(indice < size) {
-        first_array[++indice] = '\0';
-        
-        for(int i = 0; first_array[i]; i++) 
-            cout << first_array[i];
-            
-    }
-
-    if(third_array != nullptr) {
-        third_array[++indice] = '\0';
-        
-        for(int i = 0; third_array[i]; i++)
-            cout << third_array[i]; 
-        cout << endl;
-
-    }
-
-
+    for(int i = 0; first_array[i]; i++)
+		cout << first_array[i];
+    delete[] first_array;
+    first_array = nullptr;
+    
     return 0;
 }
