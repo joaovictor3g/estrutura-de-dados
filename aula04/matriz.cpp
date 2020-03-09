add01b4be686687dceabff12bc18013aa0b108af
joaovictor3g <jvdias79797@gmail.com>
2020-03-09 13:01:40 -0300

    adicoes

--- aula04/matriz.cpp
+++ aula04/matriz.cpp
@@ -6,6 +6,7 @@ void fillArray(int **mat, int nl, int nc) {
     for(int i = 0; i < nl; i++) {
         for(int j = 0; j < nc; j++) 
             cin >> mat[i][j];
+    
     }
 }
 
@@ -35,16 +36,22 @@ int main() {
     cin >> nl >> nc;
     int **matriz = new ( nothrow ) int*[nl];
 
-    for(int i = 0; i < nl; i++)
-        matriz[i] = new int[nc];
-
-    if(matriz != nullptr) {
-        fillArray(matriz, nl, nc);
-        cout << "O maior elemento da matriz é: " <<  maiorElemento(matriz, nl, nc) << endl;
-        imprimirMatriz(matriz, nl, nc);
-
-    } else 
+    if(matriz == nullptr) {
         cout << "Matriz não foi alocada" << endl;
+        exit(1);    
+
+    }  
+    for(int i = 0; i < nl; i++) {
+        matriz[i] = new ( nothrow ) int[nc]; 
+        if(matriz[i] == nullptr) {
+            cout << "Erro de memória" << endl;
+            exit(1);
+        }
+    }
+    fillArray(matriz, nl, nc);
+    cout << "O maior elemento da matriz é: " <<  maiorElemento(matriz, nl, nc) << endl;
+    imprimirMatriz(matriz, nl, nc); 
+    
     
     for(int i = 0; i < nl; i++)
         delete[] matriz[i];
