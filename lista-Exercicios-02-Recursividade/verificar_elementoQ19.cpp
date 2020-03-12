7e70a12cb7fe0347c824245e3d7a17916b807e1c
joaovictor3g <jvdias79797@gmail.com>
2020-03-12 15:09:33 -0300

    questão 19 da lista

---
+++ lista-Exercicios-02-Recursividade/verificar_elementoQ19.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int verificar_se_contem(int *array, int size, int elemento) {
+    if(size == 0)
+        return 0;
+    if(array[size-1] == elemento)
+        return 1;
+    verificar_se_contem(array, size-1, elemento);
+}
+
+int main() {
+    int size = 0;
+    cin >> size;
+
+    int *array = new (nothrow) int[size];
+    for(int i = 0; i < size; i++)
+        cin >> array[i];
+    
+    int elemento = 0;
+    cin >> elemento;
+
+    cout << verificar_se_contem(array, size, elemento);
+
+    delete[] array;
+    return 0;
+}
