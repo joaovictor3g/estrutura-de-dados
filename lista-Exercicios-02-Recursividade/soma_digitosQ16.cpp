fcc218965aadefdf8051f2306128870caa1fb85c
joaovictor3g <jvdias79797@gmail.com>
2020-03-12 14:32:27 -0300

    questões da lista 2 de recursão

---
+++ lista-Exercicios-02-Recursividade/soma_digitosQ16.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int soma_digitos(int num) {
+    if(num < 10) {
+        cout << num << "=";
+        return num;
+        
+    }
+    cout << num%10 << "+";
+    return num%10 + soma_digitos(num/10);
+    
+}
+
+int main() {
+    int num = 0;
+    cin >> num;
+    cout << soma_digitos(num) << endl;
+
+
+    return 0;
+}
