f1d65e6c9a0093632b4d3798fb51eb030bb1e17a
joaovictor3g <jvdias79797@gmail.com>
2020-03-11 22:20:26 -0300

    questão recursiva do moodle

--- moodle-exercicios/recursividade/contachar.cpp
+++ moodle-exercicios/recursividade/contachar.cpp
@@ -5,10 +5,8 @@ using namespace std;
 // Retorna o números de ocorrências do caractere 'c' na string 's' (com 'n' caracteres).
 // Algoritmo deve ser recursivo e sem comandos de repetição.
 int conta_char_rec(char s[], int n, char c) {
-    static int i = 0;
-    if(s[n-1] == c) i++;
-    if(n == 0)
-        return i;
+    if(n == 0)  return 0;
+    if(s[n-1] == c) return conta_char_rec(s, n-1, c) + 1;
     return conta_char_rec(s, n-1, c);
 
 }
--- moodle-exercicios/recursividade/triangulodesomas.cpp
+++ moodle-exercicios/recursividade/triangulodesomas.cpp
@@ -2,18 +2,47 @@
 
 using namespace std;
 
-int *soma(int *v, int n) {
-    return v;
+void print_vet(int *vet, int size) {
+    cout << "[";
+    for(int i = 0; i < size - 1; i++) {
+        cout << vet[i];
+
+        if(i < size-2)
+            cout << ", ";
+    }
+    cout << "]\n";
+}
+
+void triangulo_de_somas(int *v, int n) {
+    if(n == 1) 
+        return;
+    
+    int novo_vet[n];
+    for(int i = 0; i < n - 1; i++)
+        novo_vet[i] = v[i] + v[i+1];
+    triangulo_de_somas(novo_vet, n - 1);
+    print_vet(novo_vet, n);
     
 }
 
 int main() {
     int n = 0;
     cin >> n;
+
     int *arr = new int[n];
-    
     for(int i = 0; i < n; i++)
         cin >> arr[i];
 
+    triangulo_de_somas(arr, n);
+    cout << "[";
+    for(int i = 0; i < n; i++) {
+        cout << arr[i];
+        if(i < n-1)
+            cout <<", ";
+    }
+    cout << "]" << endl;
+    
+    
+    delete[] arr;
     return 0;
-}
+} 
