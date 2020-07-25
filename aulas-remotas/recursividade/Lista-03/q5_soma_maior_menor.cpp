309fd54cbb537c848d0820947126769f06013f50
joaovictor3g <jvdias79797@gmail.com>
2020-07-25 15:22:00 -0300

    refazendo as questoes da lista dois de ponteiro

--- aulas-remotas/recursividade/Lista-03/q5_soma_maior_menor.cpp
+++ aulas-remotas/recursividade/Lista-03/q5_soma_maior_menor.cpp
@@ -2,15 +2,24 @@
 
 using namespace std;
 
-int soma_maior_e_menor(int *arr, int size) {
+int menorV(int *arr, int size) {
     if(size == 0)
         return 0;
-    int menor = soma_maior_e_menor(arr, size-1);
-    int maior = soma_maior_e_menor(arr, size-1);
+    int menor = menorV(arr, size-1);
+    
     if(menor > arr[size-1])
         return arr[size-1];
+    return menor;
+}
+
+int maiorV(int *arr, int size) {
+    if(size == 0)
+        return 0;
+    int maior = maiorV(arr, size-1);
+    
     if(maior < arr[size-1])
         return arr[size-1];
+    return maior;
 }
 
 int main() {
@@ -20,6 +29,7 @@ int main() {
     int *arr = new int[size];
     for(int i = 0; i < size; i++)
         cin >> arr[i];
+    cout << maiorV(arr, size) - menorV(arr, size) << endl;
 
     delete[] arr;
     return 0;
