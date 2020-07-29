7009b10bc99c29b41f412efae7ed4a54c01341c8
joaovictor3g <jvdias79797@gmail.com>
2020-07-29 16:16:37 -0300

    update

--- aulas-remotas/recursividade/Lista-03/q5_soma_maior_menor.cpp
+++ aulas-remotas/recursividade/Lista-03/q5_soma_maior_menor.cpp
@@ -2,34 +2,28 @@
 
 using namespace std;
 
-int menorV(int *arr, int size) {
+void soma_maior_menor(int *arr, int size, int *max, int *min) {
     if(size == 0)
-        return 0;
-    int menor = menorV(arr, size-1);
-    
-    if(menor > arr[size-1])
-        return arr[size-1];
-    return menor;
-}
-
-int maiorV(int *arr, int size) {
-    if(size == 0)
-        return 0;
-    int maior = maiorV(arr, size-1);
-    
-    if(maior < arr[size-1])
-        return arr[size-1];
-    return maior;
+        return;
+    if(arr[size-1] >= *max) 
+        *max = arr[size-1];
+    if(arr[size-1] < *min)
+        *min = arr[size-1];
+    soma_maior_menor(arr, size-1, max, min);
 }
 
 int main() {
-    int size = 0;
+    int size = 0, max = 0, min = 0;
     cin >> size;
 
     int *arr = new int[size];
     for(int i = 0; i < size; i++)
         cin >> arr[i];
-    cout << maiorV(arr, size) - menorV(arr, size) << endl;
+    max = arr[0];
+    min = arr[0];
+
+    soma_maior_menor(arr, size, &max, &min);
+    cout << max - min << endl;
 
     delete[] arr;
     return 0;
